0ee23ba26e81a303b0f05233bb6c316410fe8302
Greg Answer <greganswer@gmail.com>
2017-03-20 02:02:26 -0400

    try redirect account to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, path: :account, controllers: { registrations: 'registrations' }
+  {
+   "account" => "/",
+ }.each { |current, towards| get current, to: redirect(towards) }
+
   resources :notes
   root "home#index"
 end
