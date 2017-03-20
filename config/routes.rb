0edfebfb8f3f0c5d293f78258d517f3282541af6
Greg Answer <greganswer@gmail.com>
2017-03-20 02:09:58 -0400

    Try remove account

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: :account, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations' }
   {
    "account" => "/",
  }.each { |current, towards| get current, to: redirect(towards) }
