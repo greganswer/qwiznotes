59e29f0883e5bc9db53dc5e5ccb3ef21952b4b7b
Greg Answer <greganswer@gmail.com>
2017-03-20 01:42:35 -0400

    Try devise redirect to root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,14 @@ class ApplicationController < ActionController::Base
   ## DEVISE
 
   def after_inactive_sign_up_path_for(resource_or_scope)
-    session.fetch 'user_return_to', root_path
+     root_path
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    session.fetch 'user_return_to', root_path
+     root_path
   end
 
   def after_sign_up_path_for(resource_or_scope)
-    session.fetch 'user_return_to', root_path
+     root_path
   end
 end
