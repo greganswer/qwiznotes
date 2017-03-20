f298b1d0400fb442a7d8b0e54896a01fd781646f
Greg Answer <greganswer@gmail.com>
2017-03-20 01:38:23 -0400

    add after_inactive_sign_up_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,21 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def hashid_decode(id)
+    hashids.decode(id)&.first
+  end
+
+  ## DEVISE
+
+  def after_inactive_sign_up_path_for(resource_or_scope)
+    session.fetch 'user_return_to', root_path
+  end
+
   def after_sign_in_path_for(resource_or_scope)
     session.fetch 'user_return_to', root_path
   end
 
-  def hashid_decode(id)
-    hashids.decode(id)&.first
+  def after_sign_up_path_for(resource_or_scope)
+    session.fetch 'user_return_to', root_path
   end
 end
