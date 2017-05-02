6aa346d1b3501777f7579deb0dc1977f0182fb06
Greg Answer <greganswer@gmail.com>
2017-05-01 20:56:14 -0400

    readjust smtp settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   protect_from_forgery with: :exception
-  # around_action :with_timezone
+  around_action :with_timezone
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :current_user
